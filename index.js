ee3c477fb9a5b7102ec23198c7fcc1f55df9e3ec
smitgor <smitgor120@gmail.com>
2021-10-10 23:56:36 +0530

    main banner

--- index.js
+++ index.js
@@ -1,8 +1,23 @@
 window.addEventListener('scroll',function(e){
-    var scrolled = document.querySelector('.navbar');
-    console.log(window.pageYOffset);
+    var navbar = document.querySelector('.navbar');
     if(window.pageYOffset<=1)
-        scrolled.style.borderBottom="";
+        navbar.style.borderBottom="";
     else
-        scrolled.style.borderBottom="solid";
+        navbar.style.borderBottom="solid";
+    
+    var p = document.querySelector('.moto p');
+    p.style.transition="fontSize 1s";
+    console.log(p.style);
+    if(window.pageYOffset>=550)
+    {
+        p.style.margin="auto";
+        p.style.padding="0px";
+        p.style.fontSize="60px";
+    }
+    else{
+        p.style.margin="";
+        p.style.paddingTop="30px";
+        p.style.fontSize="80px";
+    }
+
 });
